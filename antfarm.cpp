d8b495926b38938cd1b796734a4f91db6c314985
David Jackson <dwjackson@users.noreply.github.com>
2024-05-07 21:16:28 -0400

    Enlarge the screen and refactor drawing

--- antfarm.cpp
+++ antfarm.cpp
@@ -3,12 +3,14 @@
 #include "drawing.hpp"
 #include "raylib.h"
 
+const int side = 10;
+
+static void draw_scene(Drawing::Window &window, const Grids::Grid &grid, const Ants::Ant &ant);
+
 int main(void)
 {
-  const int screen_width = 800;
-  const int screen_height = 480;
-
-  const int side = 10;
+  const int screen_width = 1200;
+  const int screen_height = 720;
 
   const int height = screen_height / side;
   const int width = screen_width / side;
@@ -18,32 +20,7 @@ int main(void)
   auto window = Drawing::Window(screen_width, screen_height, "Ant Farm");
 
   while (!window.should_close()) {
-    // Draw the grid
-    {
-      auto drawing = window.draw();
-      drawing->clear_background();
-      auto rect = Drawing::Rectangle(side, side);
-
-      for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-          auto cell = grid.cell(i, j);
-          auto pos = Drawing::Point(j * side, i * side);
-          Color color;
-          switch (cell) {
-            case Grids::CellColour::CELL_WHITE:
-              color = WHITE;
-              break;
-            case Grids::CellColour::CELL_BLACK:
-              color = BLACK;
-              break;
-          }
-          drawing->rectangle(pos, rect, color);
-        }
-      }
-      
-      auto pos = Drawing::Point(ant.col() * side, ant.row() * side);
-      drawing->rectangle(pos, rect, RED);
-    }
+    draw_scene(window, grid, ant);
 
     // Update the ant state
     auto row = ant.row();
@@ -51,8 +28,35 @@ int main(void)
     ant = ant.next(grid);
     grid.invert_cell(row, col);
 
-    window.wait(0.1);
+    window.wait(0.05);
   }
 
   return 0;
+}
+
+static void draw_scene(Drawing::Window &window, const Grids::Grid &grid, const Ants::Ant &ant)
+{
+  auto drawing = window.draw();
+  drawing->clear_background();
+  auto rect = Drawing::Rectangle(side, side);
+
+  for (int i = 0; i < grid.height(); i++) {
+    for (int j = 0; j < grid.width(); j++) {
+      auto cell = grid.cell(i, j);
+      auto pos = Drawing::Point(j * side, i * side);
+      Color color;
+      switch (cell) {
+        case Grids::CellColour::CELL_WHITE:
+          color = WHITE;
+          break;
+        case Grids::CellColour::CELL_BLACK:
+          color = BLACK;
+          break;
+      }
+      drawing->rectangle(pos, rect, color);
+    }
+  }
+
+  auto pos = Drawing::Point(ant.col() * side, ant.row() * side);
+  drawing->rectangle(pos, rect, RED);
 }
